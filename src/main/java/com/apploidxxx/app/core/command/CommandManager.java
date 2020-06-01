5b1d1572330355af3fa652ae7d576e65b5ff600c
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-06-01 17:32:24 +0300

    help command implemented within CommandManager

--- src/main/java/com/apploidxxx/app/core/command/CommandManager.java
+++ src/main/java/com/apploidxxx/app/core/command/CommandManager.java
@@ -21,16 +21,32 @@ public class CommandManager {
     private final Map<List<String>, Command> commandMap = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(CommandManager.class);
 
-    public Command getCommand(String name){
+    public Command getCommand(String name) {
         return findByName(name);
     }
 
-    private Command findByName(String name){
-        for (List<String> keys : commandMap.keySet()){
+    private Command findByName(String name) {
+        if (name.equals("help")) {
+            return ((console, context) -> {
+                for (List<String> names : commandMap.keySet()) {
+
+                    console.print("\n* " + names.get(0));
+                    if (names.size() > 1) {
+                        console.print(" [ ");
+                        for (int i = 1; i < names.size(); i++) {
+                            console.print(" " + names.get(i) + (i != names.size() - 1 ? " ," : ""));
+                        }
+                        console.print(" ]");
+                    }
+                }
+            });
+        }
+
+        for (List<String> keys : commandMap.keySet()) {
             if (keys.contains(name)) return commandMap.get(keys);
         }
 
-        return (console, context) ->  console.println("Command not found");
+        return (console, context) -> console.println("Command not found");
     }
 
     public void init(String commandsPackage) {
@@ -39,10 +55,10 @@ public class CommandManager {
 
         scanner.addIncludeFilter(new AnnotationTypeFilter(Executable.class));
 
-        for (BeanDefinition bd : scanner.findCandidateComponents(commandsPackage)){
+        for (BeanDefinition bd : scanner.findCandidateComponents(commandsPackage)) {
             try {
                 processBean(bd);
-            } catch (ClassNotFoundException e){
+            } catch (ClassNotFoundException e) {
                 logger.error("Command class not found", e);
             }
         }
@@ -58,8 +74,9 @@ public class CommandManager {
         }
 
         for (Constructor<?> c : clazz.getDeclaredConstructors()) {
+            @SuppressWarnings("unchecked")
             Constructor<Command> castedConstructor = (Constructor<Command>) c;
-            Command commandInstance = null;
+            Command commandInstance;
             castedConstructor.setAccessible(true);
             if (castedConstructor.getParameterTypes().length == 0) {
                 try {
@@ -80,8 +97,9 @@ public class CommandManager {
             logger.warn(Executable.class.getSimpleName() + " annotation not found for " + clazz.getSimpleName());
             return new ArrayList<>();
         }
-        List<String> names = new ArrayList<>(List.of(executable.aliases()));
+        List<String> names = new ArrayList<>();
         names.add(executable.value());
+        names.addAll(List.of(executable.aliases()));
         return names;
 
     }
