40a93b21343444eeb19355b7d9a8ed454d3a3d8b
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-06-09 07:05:26 +0300

    integrated with ConsoleManager

--- src/main/java/com/apploidxxx/app/Application.java
+++ src/main/java/com/apploidxxx/app/Application.java
@@ -1,10 +1,13 @@
 package com.apploidxxx.app;
 
 import com.apploidxxx.app.console.Console;
+import com.apploidxxx.app.console.ConsoleManager;
 import com.apploidxxx.app.console.impl.SystemConsole;
 import com.apploidxxx.app.core.Shell;
 import com.apploidxxx.app.core.impl.DefaultShell;
 import org.jline.reader.UserInterruptException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InterruptedIOException;
 
@@ -12,16 +15,43 @@ import java.io.InterruptedIOException;
  * @author Arthur Kupriyanov on 18.02.2020
  */
 public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) throws Exception {
 
-        Console console = new SystemConsole(); // (only for bash) new DefaultConsole(System.in, System.out);
+        Console console = createConsole(args);
+
         Shell shell = new DefaultShell(console);
+
         try {
             shell.run();
         } catch (UserInterruptException | InterruptedException | InterruptedIOException e) {
             console.clearScreen();
-            System.out.println("Программа завершила свою работу");
         }
 
+    }
+
+    private static Console createConsole(String[] args) {
+        if (args.length > 0) {
+            try {
+
+                logger.info("Try to find console from args : " + args[0]);
+
+                Console console = ConsoleManager.getConsoleByName(args[0]);
+
+                logger.info("Found console : " + console.getClass().getSimpleName());
+
+                return console;
+
+
+            } catch (InstantiationException e) {
+                logger.error("Can't create console from args");
+                logger.warn("Please check your args with list of consoles: " + ConsoleManager.getConsoleNames());
+                logger.warn("Using default system console...");
+            }
+        }
+
+        return new SystemConsole();
     }
 }
